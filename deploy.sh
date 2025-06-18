8d5dfd4d3cd72179ef1aae10b1bd1d690092f467
matheus.luz <matheus.luz@corensc.gov.br>
2025-06-18 09:07:17 -0300

    test com dockerfile

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,13 @@ php artisan db:seed --class=DefaultCategorySeeder --force
 
 # Finalmente, inicia o supervisor no foreground para manter o container rodando
 exec /usr/bin/supervisord -n
+
+
+echo "==== Supervisord log ===="
+cat /var/log/supervisor/supervisord.log || echo "No supervisord log"
+
+echo "==== PHP-FPM status ===="
+ps aux | grep php-fpm
+
+echo "==== NGINX status ===="
+ps aux | grep nginx
